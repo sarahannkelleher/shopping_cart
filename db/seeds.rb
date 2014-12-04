6c3ca4090eb615434d5609711b2220eb4c97940c
Sarah Kelleher <apprentice@New-Caledonia.local>
2014-12-04 21:14:40 +1300

    delete function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name: 'Vinyl1', description: 'limited edition!', price: 13, image_url: "http://starsandletters.com/wp-content/uploads/2014/03/Bad-Blocks-Circulate-EP-1000px-72dpi.jpg")
+Product.create(name: 'Vinyl2', description: 'limited edition!', price: 13, image_url: "https://f1.bcbits.com/img/a1643970360_10.jpg")
+Product.create(name: 'Vinyl3', description: 'limited edition!', price: 13, image_url: "https://f1.bcbits.com/img/a2258295101_10.jpg")
+Product.create(name: 'Vinyl4', description: 'limited edition!', price: 13, image_url: "https://f1.bcbits.com/img/a2003872146_10.jpg")
+Product.create(name: 'Vinyl4', description: 'limited edition!', price: 13, image_url: "https://f1.bcbits.com/img/a2003872146_10.jpg")
